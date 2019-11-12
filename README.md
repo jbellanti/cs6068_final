f64b330db645eefd60f4244970efa4d44ede255a
kmfarley11 <kmfarley11>
2019-11-11 21:03:50 -0500

    implemented basic google cloud speech to text and sequential search

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # cs6068_final
-Code for the Parallel Computing final project
+____
+(Python 3) Code for the Parallel Computing final project
+
+Project Name: Multimedia Keyword Search in Parallel
+
+Members: 
+
+- Kevin Farley
+- Jordan Bellanti
+- Emma Romstadt
+- Joseph Weisbrod
+
+____
+### Set up Google Cloud Speech to Text
+The following was derived from [google's cloud's getting started page](https://cloud.google.com/speech-to-text/docs/reference/libraries#client-libraries-install-python)
+
+DISCLAIMER: the following instructions were designed for / tested on Windows but Linux etc. should be similar enough
+
+- set up a google cloud account: https://cloud.google.com/free/
+  - if you guys have trouble with this i can look into sharing my instances
+- install [gcloud sdk](https://cloud.google.com/sdk/)
+- via the installer gui create a project i.e. `speech2text-cs6068`
+- now execute the following in a command prompt post-install
+```batch
+REM NOTE: use whatever admin/project name you want just replace admin-speech2text/speech2text-cs6068 accordingly
+gcloud iam service-accounts create admin-speech2text
+gcloud projects add-iam-policy-binding speech2text-cs6068 --member "serviceAccount:admin-speech2text@speech2text-cs6068.iam.gserviceaccount.com" --role "roles/owner"
+gcloud iam service-accounts keys create admin-speech2text-cs6068.json --iam-account admin-speech2text@speech2text-cs6068.iam.gserviceaccount.com
+REM add the json file to your environment variables. temporarily you can do:
+set GOOGLE_APPLICATION_CREDENTIALS=<current path>\admin-speech2text-cs6068.json
+```
+- go ahead and try to run this code 
+```batch
+python3 main.py -h
+python3 main.py -s -v
+```
+- you may see a 403 error, if so you need to enable the api and billing
+  - don't worry the billing will take from the $300 provided with your trial account
+  - read the error message and go to the web link it describes to enable the api
+- re run `python3 main.py -s -v`
+- you should then see the following among other program output
+```
+Transcript: testing 1 2 3 test recording for Google Cloud
+Transcript:  yep
+```
---
+++ main.py
@@ -0,0 +1,134 @@
+import argparse
+import io
+import os
+import sys
+
+# Imports the Google Cloud client library
+from google.cloud import speech
+from google.cloud.speech import enums
+from google.cloud.speech import types
+
+
+# various globals (pathing etc.)
+DEFUALT_AUDIO_FILE_NAME = 'Audio.wav'
+DEFUALT_AUDIO_FILE_DIR  = 'resources'
+DEFAULT_AUDIO_FILE_PATH = os.path.join(
+                            os.path.dirname(__file__),
+                            DEFUALT_AUDIO_FILE_DIR, 
+                            DEFUALT_AUDIO_FILE_NAME)
+DEFAULT_KEYWORDS = ['test', 'yep']
+VERBOSE = False
+
+
+def vprint(*args):
+    """
+    custom print, only actually prints if VERBOSE is set globally
+    """
+    global VERBOSE
+    if VERBOSE:
+        print(*args)
+
+
+def do_speech_to_text(file_path, seq=False):
+    """Execute speech to text via google cloud's api.
+    Heavily derived from google cloud's tutorial:
+        https://cloud.google.com/speech-to-text/docs/reference/libraries
+
+    TODO: associate / maintain timestamps
+    TODO: parallel implementation
+    """
+    text_result = ''
+    if not seq:
+        print('Parallel implementation of speech to text not yet implemented.')
+        print('\tUse the -s option to try the sequention version.')
+        return text_result
+
+    # Instantiates a client
+    client = speech.SpeechClient()
+
+    # Loads the audio into memory
+    with io.open(file_path, 'rb') as audio_file:
+        content = audio_file.read()
+        audio = types.RecognitionAudio(content=content)
+
+        config = types.RecognitionConfig(
+                    encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+                    sample_rate_hertz=16000,
+                    language_code='en-US')
+
+        vprint('Attempting speech to text conversion...')
+
+        # Detects speech in the audio file
+        response = client.recognize(config, audio)
+
+        for result in response.results:
+            text_result += result.alternatives[0].transcript
+            vprint('Transcript:', result.alternatives[0].transcript)
+    
+    return text_result
+
+
+def do_text_search(text_input, keywords, seq=False):
+    """Execute text search.
+
+    TODO: associate / maintain timestamps
+    TODO: parallel implementation
+    """
+    hits = []
+
+    if not seq:
+        print('Parallel implementation of text search not yet implemented.')
+        print('\tUse the -s option to try the sequention version.')
+        return ''
+
+    vprint('keywords:', keywords)
+    vprint('text_input:', text_input)
+
+    for idx, word in enumerate(text_input.split(' ')):
+        # vprint('checking word:', word)
+        if word in keywords:
+            vprint('Hit at word index', idx)
+            hits.append([word, idx])
+
+    return hits
+
+
+def main():
+    """Main function for cs6068 final project.
+    Note: only if we end up using python... remove comment if necessary later
+    Usage: 
+        python3 main.py
+    
+    TODO: support audio conversion / splicing (stretch goal)
+    TODO: support ML / tensorflow option (stretch goal)
+    """
+    arg_parser = argparse.ArgumentParser()
+    _a = arg_parser.add_argument
+    _a('--sequential', '-ss', action='store_true', 
+        help='Execute the sequential version of the audio search feature.')
+
+    _a('--input-file', '-i', default=DEFAULT_AUDIO_FILE_PATH,
+        help='What file to execute the search feature on.')
+
+    _a('--verbose', '-v', action='store_true',
+        help='Print out more information during execution.')
+
+    _a('--keywords', '-k', default=DEFAULT_KEYWORDS,
+        help='What keywords to search for.')
+    args = arg_parser.parse_args()
+
+    global VERBOSE
+    VERBOSE = args.verbose
+
+    if not os.path.exists(args.input_file): 
+        print('ERROR: input file {} not found!' % args.input_file)
+        sys.exit(1)
+    
+    txt = do_speech_to_text(args.input_file, seq=args.sequential)
+    search_results = do_text_search(txt, args.keywords, seq=args.sequential)
+    print('search results:\n', search_results)
+
+    
+if __name__ == '__main__':
+    main()
+    
